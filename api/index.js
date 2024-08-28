c26aa3b3ee769aa40c726f75d046737c4b1f9482
Samuel Z <websamrgt@gmail.com>
2024-08-28 16:46:31 -0300

    ajustes

--- api/index.js
+++ api/index.js
@@ -6,14 +6,15 @@ require('dotenv').config();
 // Middleware para parsing de JSON
 app.use(express.json());
 
+// Servindo arquivos estáticos da pasta 'public'
+app.use(express.static('public'));
+
 // Rotas da API
 app.use('/api', playlistRoutes);
-
-// Servindo o formulário
-app.use(express.static('public'));
 
 // Inicia o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
